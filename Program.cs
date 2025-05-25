12ecfdc0dd4dd1dbd8298961645c7020dfa4ded4
mohamed tarek <moadbdelsamad@gmail.com>
2025-05-25 18:28:55 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PO1_Student_System
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
